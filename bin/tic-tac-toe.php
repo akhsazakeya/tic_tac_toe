162d12432a4d773133706e37e0d27657e45249b6
akhsazakeya <pypypym21@gmail.com>
2024-12-26 23:40:42 +0300

    Task04

--- bin/tic-tac-toe.php
+++ bin/tic-tac-toe.php
@@ -7,4 +7,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use akhsazakeya\TicTacToe\Controller\Controller;
 
 $controller = new Controller();
-$controller->startGame();
+$controller->menu();
